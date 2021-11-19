e92590e1bdff6ea00dd631b40b9de82f75fba084
unknown <vladancupric@gmail.com>
2021-11-19 18:00:52 +0100

    New

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+    return (
+    <form>
+        <input  type="text"/>
+        <button>Search</button>
+    </form>
+    )}
+export default Form;
+
+
+
+
