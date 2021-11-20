6e1c3f148685ca9c5440974d7815772abff0680e
unknown <vladancupric@gmail.com>
2021-11-20 18:49:02 +0100

    New commits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component} from 'react';
 import './App.css';
-import Form from "./components/Form/Form"
+import Form from "./components/Form/Form";
+
+const API_KEY = "Your-api-key";
+
 
 class App extends Component {
 
-  getRecipe() {
-    console.log("Working")
+  getRecipe = async (e)=> {
+
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    console.log(recipeName);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes)
   }
 
   render(){
