c04df9112372740b725c34c63b9aed78f9e78676
unknown <vladancupric@gmail.com>
2021-11-20 07:51:57 +0100

    New commits

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Form from "./components/Form/Form"
 
 function App() {
+  getRecipe = ()=> {
+    
+  }
   return (
     <div className="App">
       <header className="header bg-primary p-3 mb-3">
